4063bc7a5ea1b0e232e1d28a1bec301eeca2d605
Eduardo DuQuesne <eduquesne@gmail.com>
2018-04-03 15:36:23 -0500

    Build DB File

--- db/buildOrder.js
+++ db/buildOrder.js
@@ -7,14 +7,14 @@ const db = new sqlite3.Database('db/bangazon.sqlite');
 
 module.exports.build_order_table = () => {
     return new Promise((resolve, reject) => {
-        db.run(`DROP TABLE IF EXISTS orders`);
-        db.run(`CREATE TABLE IF NOT EXISTS orders 
+        db.run(`DROP TABLE IF EXISTS [order]`);
+        db.run(`CREATE TABLE IF NOT EXISTS [order] 
         (order_id INTEGER PRIMARY KEY,
         customer_id INT,
         payment_type_id INT, 
         create_date TEXT, 
         FOREIGN KEY (customer_id) REFERENCES customer (customer_id),
-        FOREIGN KEY (payment_type_id) REFERENCES payment_type (type_id)) `,
+        FOREIGN KEY (payment_type_id) REFERENCES payment_type (type_id))`,
             () => {
                 resolve("done");
             });
