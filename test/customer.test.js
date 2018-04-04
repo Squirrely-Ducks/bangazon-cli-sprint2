859556c55878060e4ea32a7849ae90d1fddc8fd6
kylesaldana5 <kylesaldana5@gmail.com>
2018-04-04 13:58:15 -0500

    finished test for creat a customer act

--- test/customer.test.js
+++ test/customer.test.js
@@ -1,24 +1,59 @@
-const { } = require('../app/models/customer');
-const { assert: { isNumber, isObject, equal } } = require('chai');
+const { new_customer, get_one_customer } = require('../app/models/Customer');
+const { assert: { isNumber, isArray, isObject, equal, isFunction, deepEqual } } = require('chai');
 
-// Placed here to confirm test file runs properly
-describe('just a test', () => {
-  it('should be equal', () => {
-    equal( 3, 1 + 2)
-  });
-});
+// Dummy arrays and objects
+let addTestObj = {
+  first_name: 'cookie',
+  last_name: 'case',
+  street: 'monster',
+  city: 'bird town',
+  state: 'sessy',
+  zip: '42069',
+  phone: '808080'
+}
+
+let oneCustomer = {
+  customer_id: 1,
+  first_name: 'Fred',
+  last_name: 'Smith',
+  street: '500 Somewho Lane',
+  city: 'St. Louis',
+  state: 'Iowa',
+  zip: '12345',
+  phone: "123-555-5309"
+}
 
 // Test for the creating customers method
 describe('customers module', () => {
+
+  describe('getting customer', () => {
+    it('return one customer from database', () => {
+      let test_id = oneCustomer.customer_id;
+      return get_one_customer(test_id)
+      .then((customer)=>{
+        deepEqual(oneCustomer, customer[0])
+      })
+    });
+  });
+
   describe('adding customer', () => {
     it('should be a function', () => {
-      isFunction();
+      isFunction(new_customer);
     });
-    it('should add an object', () => {
-      isObject();
+    it('should add an object inside array', () => {
+      isObject(addTestObj);
     });
-    it('should have an id === id', () => {
-
+    it('should post to the data base', () => {
+        return new_customer(addTestObj)
+        .then((id)=>{
+          addTestObj.customer_id = id
+          return get_one_customer(id);
+        })
+        .then((customer)=>{
+          deepEqual(customer[0], addTestObj)
+        })
     });
   });
 });
+               
+          
