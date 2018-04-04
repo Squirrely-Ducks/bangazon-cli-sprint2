8e844810dd86b3bcb6ee0bf716a2c0e0a6754e75
MyMac <ben.marcantel@gmail.com>
2018-04-04 13:57:55 -0500

    get products test ready?

---
+++ app/models/Products.js
@@ -0,0 +1,12 @@
+const { Database } = require('sqlite3').verbose();
+const path = require('path');
+
+const prodObj = [{product_id:1, seller_id:1, product_type_id:1, title:"shoe",price:"6.99", quantity:1 }]
+
+module.exports.getAllProducts = (id)=>{
+    return new Promise((resolve, reject) =>{ 
+      resolve(prodObj)   
+    });
+};
+
+
---
+++ test/product.test.js
@@ -0,0 +1,38 @@
+// import { get } from 'https';
+
+const { assert: {equal,deepEqual,isNaN,isArray, isFunction} } = require('chai');
+const { updateProduct,getAllProducts } = require('../app/models/Products.js')
+
+  
+describe('get products suite',()=>{
+  const activeCustomer = {id: 2};
+  const prodObj = [{product_id:1, seller_id:1, product_type_id:1, title:"shoe",price:"6.99", quantity:1 }]
+
+  it('it should be a function', ()=>{
+   isFunction( getAllProducts )
+  });
+  it('it should return an array',()=>{
+    return getAllProducts(activeCustomer)
+    .then((data)=>{
+      isArray(data);
+    })
+  })
+  it('the array should contain objects',()=>{
+    return getAllProducts(activeCustomer)
+    .then((data)=>{
+      let expected = prodObj
+      deepEqual( data[0], expected[0] )
+    })
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
