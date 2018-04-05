cca00aa2e45206577c116e31e45a192d9881a703
MyMac <ben.marcantel@gmail.com>
2018-04-05 09:37:46 -0500

    get one and all products ready, just need update tests

--- app/models/Products.js
+++ app/models/Products.js
@@ -1,17 +1,66 @@
 const { Database } = require('sqlite3').verbose();
 const path = require('path');
+const db = new Database(path.join(__dirname, '../..', 'db', 'bangazon.sqlite'));
 
-const prodObj = [{product_id:1, seller_id:1, product_type_id:1, title:"shoe",price:"6.99", quantity:1 }]
+const prodObj =[{product_id:1, seller_id:1, product_type_id:1, title:"shoe",price:"6.99", description:"classy fun", create_date:"2017-09-01", quantity:1 }]
 
 module.exports.getAllProducts = (id)=>{
     return new Promise((resolve, reject) =>{ 
-      resolve(prodObj)   
+      db.all(
+        `SELECT *
+        FROM product`,
+        (err, product) =>{
+            if(err) return reject(err);
+            resolve(product);
+        }); 
     });
 };
 
-module.exports.newProduct = ()=>{
+module.exports.getOneProduct = (id) =>{
+  return new Promise((resolve, reject) =>{
+      db.get(
+          `SELECT *
+          FROM product
+          WHERE product_id = "${id}" `,
+          (err, product) => {
+              if (err) return reject(err)
+              resolve(product)
+          });
+  });
+};
+
+module.exports.newProduct = (seller_id, product_type_id, title, price, description, create_date, quantity)=>{
   return new Promise((resolve, reject) =>{ 
-    resolve(prodObj)   
+    db.run(` INSERT INTO product VALUES (
+
+      null,
+      ${seller_id},
+      ${product_type_id},
+      "${title}",
+      "${price}",
+      "${description}",
+      "${create_date}",
+      ${quantity}
+      ) `, function(err, product) {
+          if (err) {
+              console.log(err);
+              reject(err);
+          }
+        resolve( this.lastID )
+    });
+  });
+};  
+
+module.exports.updateProduct = (id, columns, values) => {
+  return new Promise((resolve, reject) => {
+      for (let i = 0; i < columns.length; i++) {
+          db.run(`UPDATE product
+              SET "${columns[i]}" = "${values[i]}"
+              WHERE product.product_id = ${id}`, 
+              function (err, rows) {
+                  resolve(this.changes);
+              });
+      }
   });
 };
 
--- test/product.test.js
+++ test/product.test.js
@@ -1,12 +1,12 @@
-// import { get } from 'https';
 
-const { assert: {equal,deepEqual,isNaN,isArray, isFunction} } = require('chai');
-const { newProduct,getAllProducts } = require('../app/models/Products.js')
+
+const { assert: {equal,deepEqual,isNaN,isArray, isFunction, isNumber, isObject} } = require('chai');
+const { newProduct,getAllProducts, getOneProduct, updateProduct } = require('../app/models/Products.js')
 
   
 describe('get products suite',()=>{
-  const activeCustomer = {id: 2};
-  const prodObj = [{product_id:1, seller_id:1, product_type_id:1, title:"shoe",price:"6.99", quantity:1 }]
+  const activeCustomer = {id: 6};
+  const prodObj = [{product_id:11, seller_id:1, product_type_id:1, title:"shoe",price:"6.99", description:"classy fun", create_date:"2017-09-01", quantity:1 }]
 
   it('it should be a function', ()=>{
    isFunction( getAllProducts )
@@ -17,24 +17,63 @@ describe('get products suite',()=>{
       isArray(data);
     })
   })
-  it('the array should contain objects',()=>{
+  it('the array should contain specific object keys/values',()=>{
     return getAllProducts(activeCustomer)
     .then((data)=>{
       let expected = prodObj
-      deepEqual( data[0], expected[0] )
+      deepEqual( data[10], expected[0] )
     })
   });
 });
 
+describe('get one product suite',()=>{
+  const prodObj = [{product_id:11, seller_id:1, product_type_id:1, title:"shoe",price:"6.99", description:"classy fun", create_date:"2017-09-01", quantity:1 }]
+  it('it should be a function', ()=>{
+    isFunction( getOneProduct )
+   });
+   it('should return an object', () => {
+    return getOneProduct(11)
+        .then(data => {
+            isObject(data)
+        });
+  });
+  it('should return the correct object', () => {
+    return getOneProduct(11)
+        .then(data => {
+            deepEqual(data, prodObj[0])
+        });
+  });
+})
+
+
 describe('add products suite',()=>{
   const activeCustomer = {id: 2};
-  const prodObj = [{product_id:1, seller_id:1, product_type_id:1, title:"shoe",price:"6.99", quantity:1 }]
-
+ 
   it('it should be a function',()=>{
-    isFunction( newProduct )
-  })
-  // it('')
+    isFunction( newProduct );
+  });
+  it('should return a number',()=>{
+    return newProduct( 1, 1, "shoe", "6.99", "classy fun", "2017-09-01", 1)
+    .then(productId=>{
+      isNumber(productId);
+    })
+  });
+  it('should create the correct object in the database', () => {
+    return newProduct( 2, 1, "cookie", "3.50", "good eats", "2017-09-03", 1)
+        .then(productId => {
+            return getOneProduct(11)
+        .then(newProd => {
+            equal(11, newProd.product_id)
+        });
+
+        });
+  });
+});
+
+describe('update products suite',()=>{
+
 })
+//update: get all prods by cus8tomer id, update selected product
 
 
 
