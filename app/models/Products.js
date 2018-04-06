5b154422f1e90e6932e3e538b566472979cd3008
MyMac <ben.marcantel@gmail.com>
2018-04-05 21:30:08 -0500

    added get product by customer/seller id

--- app/models/Products.js
+++ app/models/Products.js
@@ -29,8 +29,32 @@ module.exports.getOneProduct = (id) =>{
   });
 };
 
+module.exports.getAllProductsByCust = (custId)=>{
+    return new Promise((resolve, reject) =>{ 
+      db.all(
+        `SELECT *
+        FROM product
+        WHERE seller_id = ${custId}`,
+        (err, product) =>{
+            if(err) return reject(err);
+            resolve(product);
+        }); 
+    });
+};
 
-
+module.exports.getOneProductByCust = (prodId,custId) =>{
+    return new Promise((resolve, reject) =>{
+        db.get(
+            `SELECT *
+            FROM product
+            WHERE product_id = ${prodId} 
+            AND seller_id = ${custId} `,
+            (err, product) => {
+                if (err) return reject(err)
+                resolve(product)
+            });
+    });
+  };
 
 module.exports.newProduct = (seller_id, product_type_id, title, price, description, create_date, quantity)=>{
   return new Promise((resolve, reject) =>{ 
@@ -54,25 +78,24 @@ module.exports.newProduct = (seller_id, product_type_id, title, price, descripti
   });
 };  
 
-module.exports.updateProduct = (id, column, values) => {
+module.exports.updateProduct = (id, columns, values) => {
     return new Promise((resolve, reject) => {
-        console.log("yo", id, column, values )
         
-        for (let i = 0; i < column.length; i++) {
+        for (let i = 0; i < columns.length; i++) {
             db.run(`UPDATE product
-                SET "${column[i]}" = "${values[i]}"
+                SET "${columns[i]}" = "${values[i]}"
                 WHERE product_id = ${id}`, 
                 function (err, rows) {
                     resolve();
                 });
-        }
+        };
     });
 };
 
 module.exports.deleteProduct = (id) => {
     return new Promise((resolve, reject) => {
         db.run (` DELETE FROM product
-        WHERE product.product_id=${id}`,
+        WHERE product.product_id = ${id}`,
             (err, type) => {
                 if (err) return reject(err);
                 resolve(this);
