a034545f639fa974f31f8e54631b88c525b60c89
Eduardo DuQuesne <eduquesne@gmail.com>
2018-04-10 09:45:12 -0500

    Fix Conflicts

--- app/ui.js
+++ app/ui.js
@@ -16,12 +16,21 @@ const {
 const { new_customer } = require("./models/Customer");
 const db = new Database(path.join(__dirname, "..", "db", "bangazon.sqlite"));
 const { setActiveCustomer, getActiveCustomer } = require("./activeCustomer");
+const {promptNewCustomer,promptAllCustomers} = require("./controllers/customerCtrl");
+const { new_customer } = require("./models/Customer");
+const db = new Database(path.join(__dirname, "..", "db", "bangazon.sqlite"));
+const { setActiveCustomer, getActiveCustomer } = require("./activeCustomer");
+const {promptAddPayment,createPayment} = require("./controllers/addpaymentCtrl");
+const {promptAddToCart, addNewProduct, updateProductArray, updateProducts, removeProduct} = require("./controllers/productCtrl");
 const { displayRevReport } = require("./controllers/revenueCtrl");
+
 // Start Program
 prompt.start();
 
 // Main Menu Options for CLI
 let mainMenuHandler = (err, userInput) => {
+  let id = getActiveCustomer().id
+
   // Allows user to Create a new customer and push to db
   switch (userInput.choice) {
     case "1":
@@ -43,16 +52,46 @@ let mainMenuHandler = (err, userInput) => {
       });
       break;
     //Allow User to Show Revenue Report
+    case "3":
+      promptAddPayment().then(paydata => {
+        createPayment(paydata);
+        module.exports.displayWelcome();
+      });
+      break
+        // Add product to shopping cart
+    case "4":
+      promptAddToCart(id)   
+      break;
+
     case "6":
+      // Add a Product to Sell
+      addNewProduct(id)
+      //main menus prompts
+      
+      break;
+  
+    case "7":
+      // Update a Product to Sell
+      
+      updateProductArray(id)
+      break;
+
+    case "8":
+    // Remove a Product to Sell
+    
+      removeProduct(id);
+      break;
+    case "9":
       displayRevReport().then(done => {
-        if (!done) {
+          if (!done) {
           console.log("");
           console.log(colors.red("THIS USER HAS NO SALES"));
           console.log("");
-        } else {
-          backToMenu();
-        }
-      });
+          } else {
+            backToMenu();
+          }
+        });
+        break;  
   }
 };
 
@@ -72,8 +111,11 @@ module.exports.displayWelcome = () => {
   ${colors.america("3.")} ${colors.white("Create a payment option")}
   ${colors.america("4.")} ${colors.white("Add product to shopping cart")}
   ${colors.america("5.")} ${colors.white("Complete an order")}
-  ${colors.america("6.")} ${colors.white("Get revenue rep")}
-  ${colors.america("7.")} ${colors.white("Leave Bangazon!")}`);
+  ${colors.america("6.")} ${colors.white("Add a Product to Sell")}
+  ${colors.america("7.")} ${colors.white("Update a Product to Sell")}
+  ${colors.america("8.")} ${colors.white("Remove a Customer Product from inventory")}
+  ${colors.america("9.")} ${colors.white("Get revenue rep")}
+  ${colors.america("10.")} ${colors.white("Leave Bangazon!")}`);
     prompt.get(
       [
         {
